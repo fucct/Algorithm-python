08316a99b2086f2b5e801e6f9304e0aba049e88a
dd <dqrd123@gmail.com>
2020-08-30 02:11:09 +0900

    solve: leetcode #2

---
+++ leetcode2.py
@@ -0,0 +1,47 @@
+from leetcode21 import ListNode
+
+
+class Solution2:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        result = []
+        adder = 0
+        while l1 and l2:
+            num = l1.val + l2.val + adder
+            if num >= 10:
+                result.append(num-10)
+                adder = 1
+            else:
+                result.append(num)
+                adder = 0
+            l1 = l1.next
+            l2 = l2.next
+        while l1:
+            num = l1.val + adder
+            if num >= 10:
+                result.append(num - 10)
+                adder = 1
+            else:
+                result.append(num)
+                adder = 0
+            l1 = l1.next
+        while l2:
+            num = l2.val + adder
+            if num >= 10:
+                result.append(num - 10)
+                adder = 1
+            else:
+                result.append(num)
+                adder = 0
+            l2 = l2.next
+        if adder == 1:
+            result.append(adder)
+
+        if not result:
+            resultNode = ListNode()
+            return resultNode
+        num = result.pop()
+        resultNode = ListNode(num)
+        while result:
+            num = result.pop()
+            resultNode = ListNode(num, resultNode)
+        return resultNode
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+from leetcode2 import Solution2
+from leetcode21 import ListNode
 from leetcode28 import Solution28
 
-sol = Solution28()
-print(sol.strStr("mississippi", "issip"))
+sol = Solution2()
+l1 = ListNode(2, ListNode(4, ListNode(3)))
+l2 = ListNode(5, ListNode(6, ListNode(4)))
+sol.addTwoNumbers(l1, l2).print()
