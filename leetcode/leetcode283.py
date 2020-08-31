f1cb746d74f936ab2a29f21954135223828e5a16
dd <dqrd123@gmail.com>
2020-09-01 04:24:01 +0900

    solve: leetcode #283

---
+++ leetcode/leetcode283.py
@@ -0,0 +1,15 @@
+from typing import List
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        idx = 0
+        for num in nums:
+            if num is not 0:
+                nums[idx] = num
+                idx += 1
+        for i in range(idx, len(nums)):
+            nums[i] = 0
