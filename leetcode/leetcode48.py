2e493e63d84e4052119b339a73efc91f5cc91209
dd <dqrd123@gmail.com>
2020-09-01 14:07:07 +0900

    solve: leetcode #48

---
+++ leetcode/leetcode48.py
@@ -0,0 +1,14 @@
+from typing import List
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        length = len(matrix)
+        half = length // 2
+        for i in range(0, half):
+            for j in range(0, length - half):
+                matrix[i][j], matrix[j][~i], matrix[~i][~j], matrix[~j][i] = matrix[~j][i], matrix[i][j], matrix[j][~i], \
+                                                                             matrix[~i][~j]
