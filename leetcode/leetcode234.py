e91e200400bde4547fe8db0b507dd7a538042d2a
dd <dqrd123@gmail.com>
2020-09-02 19:23:20 +0900

    solve: leetcode #234

--- leetcode/leetcode234.py
+++ leetcode/leetcode234.py
@@ -1,5 +1,25 @@
 from leetcode.leetcode21 import ListNode
 
 
-class Solution:
+class Solution234:
     def isPalindrome(self, head: ListNode) -> bool:
+        first = second = head
+
+        while second and second.next:
+            second = second.next.next
+            first = first.next
+
+        prev = None
+        while first:
+            curr = first.next
+            first.next = prev
+            prev = first
+            first = curr
+
+        while prev:
+            if prev.val != head.val:
+                return False
+            prev = prev.next
+            head = head.next
+
+        return True
--- main.py
+++ main.py
@@ -4,11 +4,12 @@ from leetcode.leetcode189 import Solution189
 from leetcode.leetcode19 import Solution19
 from leetcode.leetcode206 import Solution206
 from leetcode.leetcode21 import ListNode
+from leetcode.leetcode234 import Solution234
 from leetcode.leetcode242 import Solution242
 from leetcode.leetcode38 import Solution38
 from leetcode.leetcode387 import Solution387
 from leetcode.leetcode7 import Solution7
 
-sol = Solution206()
+sol = Solution234()
 
-sol.reverseList(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))))
+sol.isPalindrome(ListNode(1, ListNode(2, ListNode(3, ListNode(4, ListNode(5))))))
