8afcb970d7f9b6564febd35cb6d7acdec62e777d
dd <dqrd123@gmail.com>
2020-09-04 16:05:00 +0900

    solve: leetcode #88

--- leetcode/leetcode88.py
+++ leetcode/leetcode88.py
@@ -1,3 +1,4 @@
+from cmath import inf
 from typing import List
 
 
@@ -5,30 +6,19 @@ class Solution88:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
         first = m - 1
         second = n - 1
-        if n == 0:
-            return
-        for idx in range(1, len(nums1) + 1):
-            index = idx * -1
+        index = -1
 
+        while first >= 0 and second >= 0:
             if nums1[first] >= nums2[second]:
                 nums1[index] = nums1[first]
-                nums1[first] = 0
+                nums1[first] = float(-inf)
                 first -= 1
             else:
                 nums1[index] = nums2[second]
-                nums2[second] = 0
                 second -= 1
-            if first < 0 or second < 0:
-                break
+            index -= 1
 
-        if first >= 0 and idx <= len(nums1):
-            for i in range(idx + 1, len(nums1) + 1):
-                index = i * -1
-                nums1[index] = nums1[first]
-                first -= 1
-
-        if second >= 0 and idx <= len(nums1):
-            for i in range(idx + 1, len(nums1) + 1):
-                index = i * -1
-                nums1[index] = nums2[second]
-                second -= 1
+        while second >= 0:
+            nums1[index] = nums2[second]
+            index -= 1
+            second -= 1
